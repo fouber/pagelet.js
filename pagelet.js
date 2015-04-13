c4a23e961c13eb44904e093d7f307bf15a53084f
mansonchor@126.com <mansonchor@126.com>
2015-04-13 17:03:13 +0800

    Revert "asf"
    This reverts commit 47936307eaf8f854a8f81ba3a2b63925051a0d37.

--- pagelet.js
+++ pagelet.js
@@ -1,309 +1,213 @@
-;(function(global){
-	var pagelet = global.pagelet = {};
-	var loaded = {};
-	var isOldWebKit = +navigator.userAgent.replace(/.*AppleWebKit\/(\d+)\..*/, '$1') < 536;
-	var head = document.head || document.getElementsByTagName('head')[0];
-	var TIMEOUT = 60 * 1000;
-	var combo = false;
-	var DEFAULT_COMBO_PATTERN = '/co??%s';
-	var comboPattern = DEFAULT_COMBO_PATTERN;
-	var supportPushState = global.history && global.history.pushState && global.history.replaceState && !navigator.userAgent.match(/((iPod|iPhone|iPad).+\bOS\s+[1-4]\D|WebApps\/.+CFNetwork)/);
-	var _state,_xhr
+/**
+ *  Pagelet main module
+ */
 
-	function noop(){}
-	function filter(item){ return !!item; }
-	function is(obj, type){
-		return Object.prototype.toString.call(obj).toLowerCase() === '[Object ' + type + ']'.toLowerCase();
-	}
+var $document = document
+var hist = global.history
+var comboPattern = DEFAULT_COMBO_PATTERN;
+var combo = false; // 是否采用combo
+var loaded = {};
+// 是否支持Html5的PushState
+var supportPushState =
+    hist && hist.pushState && hist.replaceState &&
+    // pushState isn't reliable on iOS until 5.
+    !navigator.userAgent.match(/((iPod|iPhone|iPad).+\bOS\s+[1-4]\D|WebApps\/.+CFNetwork)/);
 
-	pagelet.init = function(cb, cbp, used){
-		combo = !!cb;
-		comboPattern = cbp || DEFAULT_COMBO_PATTERN;
-		if(used && used.length){
-			used.forEach(function(uri){
-				loaded[uri] = true;
-			});
-		}
-	};
+var pagelet = global.pagelet = {};
+/**
+ *  Attature message function to pagelet instance
+ */
+messagify(pagelet);
 
-	//初始浏览器的第一个state
-	//可以用来兼容后端渲染出来的第一个页面
-	pagelet.initState = function(pagelets , pageletOptions){
+pagelet.init = function(cb, cbp, used) {
+    combo = !!cb;
+    comboPattern = cbp || DEFAULT_COMBO_PATTERN;
+    if (used && used.length) {
+        used.forEach(function(uri) {
+            loaded[uri] = true;
+        });
+    }
+};
 
-		var pagelets = pagelets || []
-		var pageletOptions = pageletOptions || {}
+pagelet.load = function(url, pagelets, callback, progress) {
+    if (pagelets && pagelets.length) {
+        callback = callback || noop;
+        progress = progress || noop;
+        if (_is(pagelets, 'string')) {
+            pagelets = pagelets.split(/\s*,\s*/);
+        }
+        var quickling = url + (url.indexOf('?') === -1 ? '?' : '&') + 'pagelets=' + encodeURIComponent(pagelets.join(','));
 
-		if(!_state){
-			_state = {
-				url: global.location.href,
-				title: document.title,
-				pagelets : pagelets,
-				pageletOptions : pageletOptions
-			};
-			global.history.replaceState(_state, document.title);
-		}
-	}
+        loader.request(quickling, {
+            before: function (xhr) {
+                pagelet.emit('beforeload', pagelets, xhr)
+            }
+        }, function (err, result) {
+            pagelet.emit('loadend', pagelets, err, result)
 
-	pagelet.go = function(url, pagelets, pageletOptions){
-		//console.log(url, pagelets, pageletOptions)
-		var stateReplace = Boolean(pageletOptions.stateReplace) || false
-		if(supportPushState && pagelets){
-			pagelet.initState()
-		}
+            if (err) return callback(err);
 
-		pagelet.load(url, pagelets, pageletOptions, function(err, data, done){
+            $document.title = result.title || $document.title;
+            var res = [];
+            _addResource(res, result.js, 'js');
+            _addResource(res, result.css, 'css');
+            var done = function() {
+                if (result.script && result.script.length) {
+                    var left = '!function(){';
+                    var right = '}();\n';
+                    var code = left + result.script.join(right + left) + right;
+                    _exec(code);
+                }
+                //TODO input[autofocus], textarea[autofocus]
+                done = noop;
+            };
 
-			//加载成功之后用带 完整state的url去replace
-			var title = data.title || document.title;
-	        var state = {
-				url: url,
-				title: title,
-				pagelets : pagelets,
-				pageletOptions : pageletOptions
-	        };
-	        global.history.replaceState(state, title, url);
-	        // Clear out any focused controls before inserting new page contents.
-        	try { document.activeElement.blur() } catch (e) { }
+            var error = err
+            if (res && res.length) {
+                var len = res.length;
+                res.forEach(function(r) {
+                    loader(r.uri, r.type, function(e) {
+                        len--;
+                        if (len === 0) {
+                            callback(error, result, done);
+                        }
+                        error = e;
+                    });
+                });
+            } else {
+                callback(error, result, done);
+            }
+        }, progress)
+    } else {
+        location.href = url;
+    }
+};
 
-        	if(err){
-        		throw new Error(err);
-        	}else{
-        		_processHtml(data.html)
-        	}
-		})
-		
-		//在发起异步请求开始时就先改变url，pagelet load完成之后再改变_state值
-		if (_xhr.readyState > 0) {
-			if(stateReplace){
-				global.history.replaceState(null, "", url);
-			}else{
-				global.history.pushState(null, "", url);
-			}
-		}
-	}
 
-	pagelet.load = function(url, pagelets, pageletOptions, callback){
-		if(pagelets && pagelets.length){
-			callback = callback || noop;
-			if(is(pagelets, 'String')){
-				pagelets = pagelets.split(/\s*,\s*/);
-			}
-			pagelets = pagelets.join(',');
-			var quickling = url + (url.indexOf('?') === -1 ? '?' : '&') + 'pagelets=' + encodeURIComponent(pagelets);
+var state; // state is a cached var for last popstate
 
-			if ( _xhr && _xhr.readyState < 4) {
-				_xhr.onreadystatechange = noop;
-				_xhr.abort();
-			}
+pagelet.go = function(url, pagelets, processHtml, progress) {
+    if (supportPushState && pagelets) {
+        if (!state) {
+            state = {
+                url: global.location.href,
+                title: $document.title
+            };
+            hist.replaceState(state, $document.title);
+        }
+        pagelet.load(url, pagelets, function(err, data, done) {
+            var title = data.title || $document.title;
+            state = {
+                url: url,
+                title: title
+            };
+            hist.replaceState(state, title, url);
+            // Clear out any focused controls before inserting new page contents.
+            try {
+                $document.activeElement.blur()
+            } catch (e) {}
+            if (processHtml(null, data.html) !== false) done();
+        }, progress);
 
-			_xhr = new global.XMLHttpRequest();
-			//_xhr.onprogress = progress;
-			_xhr.onreadystatechange = function(){
-				if(_xhr.readyState == 4) 
-				{
-					_xhr.onreadystatechange = noop;
-					var result, error = null;
-					if((_xhr.status >= 200 && _xhr.status < 300) || _xhr.status == 304){
-						result = _xhr.responseText;
-						try {
-							result = JSON.parse(result);
-						} catch (e) {
-							error = e;
-						}
-					}else {
-			            callback(_xhr.statusText || (_xhr.status ? 'error' : 'abort'));
-			        }
+        var xhr = loader.xhr()
+        if (xhr && xhr.readyState > 0) {
+            hist.pushState(null, "", url);
+        }
+    } else {
+        location.href = url;
+    }
+};
 
-					//pagelet加载完成处理
-					if(error){
-              			callback(error);
-              		}else{
-              			_pageletLoaded(result, callback)
-              		}
-				}
-			}
+pagelet.autoload = function() {
+    global.addEventListener('popstate', function(e) {
+        state = e.state;
+        if (state) {
+            location.href = state.url;
+        }
+    }, false);
 
-			_xhr.open('GET', quickling, true);
-			_xhr.send();
-		} 
-		else {
-			location.href = url;
-		}
-	}
+    $document.documentElement.addEventListener('click', function(e) {
+        var target = e.target;
+        if (target.tagName.toLowerCase() === 'a') {
+            // Middle click, cmd click, and ctrl click should open
+            // links in a new tab as normal.
+            if (e.which > 1 || e.metaKey || e.ctrlKey || e.shiftKey || e.altKey) return;
+            // Ignore cross origin links
+            if (location.protocol !== target.protocol || location.hostname !== target.hostname) return;
 
-	//pagelet加载完成
-	function _pageletLoaded(result, callback){
+            var pagelets = _attr(target, 'data-pagelets');
+            var parents = _attr(target, 'data-parents');
+            var autocache = _attr(target, 'data-autocache');
+            var href = _attr(target, 'href');
 
-		document.title = result.title || document.title;
-		var res = [],error = null;
-		_addResource(res, result.js, 'js');
-		_addResource(res, result.css, 'css');
-		var done = function(){
-			if(result.script && result.script.length){
-				var left = '!function(){';
-				var right = '}();\n';
-				var code = left + result.script.join(right + left) + right;
-				_exec(code);
-			}
-			//TODO input[autofocus], textarea[autofocus]
-			done = noop;
-		};
-		if(res && res.length){
-			var len = res.length;
-			res.forEach(function(r)
-			{
-				_loadResource(r.uri, r.type, function(err)
-				{
-					len--;
-					if(len === 0){
-						callback(error, result, done);
-					}
-					error = err;
-				});
-			});
-		} 
-		else {
-			callback(error, result, done);
-		}
-	}
+            pagelets = (pagelets || '').split(/\s*,\s*/).filter(_filter);
+            parents = (parents || '').split(/\s*,\s*/).filter(_filter);
 
-	//默认的pagelet dom替换操作
-	function _processHtml(htmlObj){
-		for(var pageletId in htmlObj){
-			if(htmlObj.hasOwnProperty(pageletId)){
-				var objTemp = document.createElement("div");
-				objTemp.innerHTML = htmlObj[pageletId];
-				var dom = document.getElementById(pageletId);
-				dom.innerHTML = objTemp.childNodes[0].innerHTML
-			}
-		}
-	}
+            if (href && parents.length === pagelets.length && pagelets.length > 0) {
+                e.preventDefault();
+                e.stopPropagation();
 
-	function _exec(code){
-		var node = document.createElement('script');
-		node.appendChild(document.createTextNode(code));
-		head.appendChild(node);
-	}
+                if (autocache === 'cached') {
+                    // 不触发pagelet请求
+                    return false;
+                }
+                if (autocache === 'false') {
+                    // 让pagelet请求带上时间戳，避免命中浏览器缓存
+                    href += (href.indexOf('?') >= 0 ? '&' : '?') + '_ts=' + Date.now();
+                }
 
-	//添加静态资源到map，会做排重处理
-	function _addResource(result, collect, type){
-		if(collect && collect.length)
-		{
-			collect = collect.filter(function(uri){
-				var has = loaded[uri] === true;
-				loaded[uri] = true;
-				return !has;
-			});
-			if(collect.length)
-			{
-				if(combo)
-				{
-					var uri = collect.join(',');
-					result.push({
-						uri: comboPattern.replace('%s', uri),
-						type: type
-					});
-				}
-				else 
-				{
-					collect.forEach(function(uri){
-						result.push({
-							uri: uri,
-							type: type
-						});
-					});
-				}
-			}
-		}
-	}
+                var map = {};
+                pagelets.forEach(function(pagelet, index) {
+                    map[pagelet] = parents[index];
+                });
+                pagelet.go(href, pagelets, function(err, html) {
+                    if (err) {
+                        throw new Error(err);
+                    } else {
+                        for (var key in html) {
+                            if (_hasOwn(html, key) && _hasOwn(map, key)) {
+                                var parent = map[key];
+                                var dom = $document.getElementById(parent);
+                                if (dom) {
+                                    dom = null;
+                                    if (autocache === 'true') {
+                                        // 下次点击不会触发pagelet请求
+                                        target.setAttribute('data-autocache', 'cached');
+                                    }
+                                } else {
+                                    throw new Error('undefined parent dom [' + parent + ']');
+                                }
+                            }
+                        }
+                    }
+                });
+            }
+        }
+    }, false);
+};
 
-	//加载静态资源
-	function _loadResource(url, type, callback){
-		var isScript = type === 'js';
-		var isCss = type === 'css';
-		var node = document.createElement(isScript ? 'script' : 'link');
-		var supportOnload = 'onload' in node;
-		var tid = setTimeout(function(){
-			clearTimeout(tid);
-			clearInterval(intId);
-			callback('timeout');
-		}, TIMEOUT);
-		var intId;
-		if(isScript){
-			node.type = 'text/javascript';
-			node.async = 'async';
-			node.src = url;
-		} else {
-			if(isCss){
-				node.type = 'text/css';
-				node.rel = 'stylesheet';
-			}
-			node.href = url;
-		}
-		node.onload = node.onreadystatechange = function(){
-			if(node && (!node.readyState || /loaded|complete/.test(node.readyState)))
-			{
-				clearTimeout(tid);
-				node.onload = node.onreadystatechange = noop;
-				if(isScript && head && node.parentNode) head.removeChild(node);
-				callback();
-				node = null;
-			}
-		};
-		node.onerror = function(e){
-			clearTimeout(tid);
-			clearInterval(intId);
-			e = (e||{}).error || new Error('load resource timeout');
-			e.message = 'Error loading [' + url + ']: ' + e.message;
-			callback(e);
-		};
-		head.appendChild(node);
-		if(isCss){
-			if(isOldWebKit || !supportOnload)
-			{
-				intId = setInterval(function()
-				{
-					if(node && node.sheet){
-						clearTimeout(tid);
-						clearInterval(intId);
-						callback();
-					}
-				}, 20);
-			}
-		}
-	}
-
-	//autoload
-	document.documentElement.addEventListener( 'click', function(e) 
-	{
-		var target = e.target;
-		while(target && target.tagName.toLowerCase()!="body")
-		{
-			if( target.tagName.toLowerCase() === 'a' ) {
-				var href = target.getAttribute('href')
-				//to array
-				var pagelets = (target.getAttribute('data-pagelets') || '').split(/\s*,\s*/).filter(filter)
-				var stateReplace = target.getAttribute('data-pagelets-stateReplace') || false
-				var nocache = target.getAttribute('data-pagelets-nocache') || false
-
-				if( href && pagelets.length > 0 ){
-					e.preventDefault()
-					e.stopPropagation()
-
-					var pageletOptions = {
-						stateReplace : stateReplace,
-						nocache : nocache
-					}
-					pagelet.go(href, pagelets, pageletOptions)
-				}
-
-				break;
-			}
-
-			target = target.parentNode
-		}
-	})
-
-	
-
-})(window);
+function _addResource(result, collect, type) {
+    if (collect && collect.length) {
+        collect = collect.filter(function(uri) {
+            var has = loaded[uri] === true;
+            loaded[uri] = true;
+            return !has;
+        });
+        if (collect.length) {
+            if (combo) {
+                var uri = collect.join(',');
+                result.push({
+                    uri: comboPattern.replace('%s', uri),
+                    type: type
+                });
+            } else {
+                collect.forEach(function(uri) {
+                    result.push({
+                        uri: uri,
+                        type: type
+                    });
+                });
+            }
+        }
+    }
+}
